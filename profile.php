5af83e1840c1c4794ad1dae764a2ecb3f37b467c
kdos <deeper.komal@gmail.com>
2013-12-20 21:57:53 +0545

    add profile.php

---
+++ profile.php
@@ -0,0 +1,19 @@
+<html>
+
+<body>
+
+<form role="form" action="textload.php" method="post">
+<div class="form-group">
+<label>
+Roll No.:
+</label>
+<input type="text" name="roll" class="form-control">
+</div>
+
+<input type="submit" name="submit" value="submit">
+
+</form>
+
+</body>
+
+</html>
